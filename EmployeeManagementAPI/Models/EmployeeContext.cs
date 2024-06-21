e32f7fccfb2e248a3f53e84de8fe9ed21fd6362b
Abirelbahri <elbahri.abir37@gmail.com>
2024-06-21 17:08:39 +0100

    Set Up the SQLite Database

---
+++ EmployeeManagementAPI/Models/EmployeeContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementAPI.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
